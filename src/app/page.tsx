77d74078252a9195189507b846d08e533a680107
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-14 20:07:48 -0300

    feat(home): add header image and basic layout for TODO app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
+import headerImage from "../assets/bg-desktop-dark.jpg";
+
 export default function Home() {
   return (
-    <main
-      className="flex flex-col align-center justify-center text-center
-    w-full h-screen bg-background"
-    >
-      <h1 className="text-foreground">Hello World</h1>
-      <h1 className="text-muted-foreground">Hello 2</h1>
+    <main className="bg-background object-cover w-full h-screen flex flex-col">
+      <header>
+        <Image
+          src={headerImage}
+          alt="header"
+          className="w-full h-[300px] object-cover"
+        />
+      </header>
+      <div className="w-[540px]  h-[540px] mx-auto mt-[-230px]">
+        <div className="flex justify-between">
+          <h1 className="top-[300px] text-white text-5xl font-bold">TODO</h1>
+          <button>toggle</button>
+        </div>
+      </div>
     </main>
   );
 }
