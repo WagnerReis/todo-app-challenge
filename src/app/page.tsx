b9195058b6b94ba0c5da529570ff67f8f2224c38
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-14 14:07:49 -0300

    setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col align-center justify-center text-center
+    w-full h-screen">
+      <h1 className="text-white">Hello World</h1>
+    </main>
+  );
+}
