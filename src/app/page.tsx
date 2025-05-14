d724c47963e3f4d96e662d146664796256cfdbb0
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-14 19:32:31 -0300

    feat(styling): update Tailwind and PostCSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex flex-col align-center justify-center text-center
-    w-full h-screen">
-      <h1 className="text-white">Hello World</h1>
+    <main
+      className="flex flex-col align-center justify-center text-center
+    w-full h-screen bg-background"
+    >
+      <h1 className="text-foreground">Hello World</h1>
+      <h1 className="text-muted-foreground">Hello 2</h1>
     </main>
   );
 }
