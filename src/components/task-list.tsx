2c5ee2a3993d006e0c1235a96e72b1046d9f36e7
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-15 11:11:34 -0300

    v

--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -3,7 +3,7 @@ import { Separator } from "./ui/separator";
 
 export function TaskList() {
   return (
-    <main className="w-full flex-grow bg-muted-background rounded-lg">
+    <main className="w-full flex-grow bg-muted-background rounded-lg shadow-xl">
       {Array.from({ length: 5 }).map((_, index) => (
         <div key={index}>
           {index % 2 === 0 ? <TaskItem /> : <TaskItem checked />}
