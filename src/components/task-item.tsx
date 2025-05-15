4628f74d506b48d942917e1f547d199e6b537a15
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-15 13:59:49 -0300

    feat(TaskItem): add delete button and improve styling

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,3 +1,4 @@
+import { X } from "@phosphor-icons/react";
 import { Button } from "./ui/button";
 
 interface TaskItemProps {
@@ -8,7 +9,14 @@ export function TaskItem({ checked = false }: TaskItemProps) {
   return (
     <div className="w-full h-16 bg-muted-background rounded-lg pl-6 flex items-center gap-6">
       <Button checked={checked} onClick={() => console.log("clicou")} />
-      <span className="text-foreground cursor-custom">TaskItem</span>
+      <span
+        className={`text-foreground text-[12px] cursor-custom ${checked ? "line-through text-muted-foreground" : ""}`}
+      >
+        TaskItem
+      </span>
+      <button className="ml-auto mr-6 cursor-custom">
+        <X size={24} weight="thin" />
+      </button>
     </div>
   );
 }
