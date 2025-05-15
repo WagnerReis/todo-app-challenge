d64d476c8b84f032a6059dc74100774a5c15e1e2
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-15 14:40:58 -0300

    feat(navbar): add status selection functionality

--- src/components/navbar-status.tsx
+++ src/components/navbar-status.tsx
@@ -1,16 +1,34 @@
 "use client";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useState } from "react";
 
-const statusOptions = ["All", "Active", "Completed"];
+type StatusType = "All" | "Active" | "Completed";
+
+const statusOptions: StatusType[] = ["All", "Active", "Completed"];
 
 const baseClass =
-  "text-[14px] text-gray-400 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-300 active:text-blue-600 dark:active:text-blue-600 cursor-custom";
+  "text-[14px] text-gray-400 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-300 cursor-custom font-bold";
 
-function NavItem({ label }: { label: string }) {
-  return <span className={baseClass}>{label}</span>;
+interface NavItemProps {
+  label: StatusType;
+  selected: StatusType;
+  onClick: () => void;
+}
+
+function NavItem({ label, onClick, selected }: NavItemProps) {
+  console.log("la");
+  return (
+    <button
+      className={`${baseClass} ${selected === label ? "text-violet-600 dark:text-violet-600" : ""}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 }
 
 export function NavbarStatus() {
+  const [selected, setIsSelected] = useState<StatusType>("All");
   const isMobile = useIsMobile();
 
   const mobileBaseClass = isMobile ? "bg-muted-background mt-4 rounded-lg" : "";
@@ -20,7 +38,12 @@ export function NavbarStatus() {
       className={`w-full h-12 flex gap-[18px] justify-center items-center ${mobileBaseClass}`}
     >
       {statusOptions.map((status) => (
-        <NavItem key={status} label={status} />
+        <NavItem
+          key={status}
+          label={status}
+          selected={selected}
+          onClick={() => setIsSelected(status)}
+        />
       ))}
     </nav>
   );
