a4a4016db8110b901d9244149435c813154350e5
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-15 15:08:07 -0300

    feat(task-list): implement drag and drop functionality

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,16 +1,33 @@
 import { X } from "@phosphor-icons/react";
 import { Button } from "./ui/button";
+import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
 
 interface TaskItemProps {
+  id: string;
   checked?: boolean;
 }
 
-export function TaskItem({ checked = false }: TaskItemProps) {
+export function TaskItem({ id, checked = false }: TaskItemProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <div className="w-full h-16 bg-muted-background rounded-lg pl-6 flex items-center gap-6">
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className="w-full h-16 bg-muted-background rounded-lg pl-6 flex items-center gap-6 cursor-custom"
+    >
       <Button checked={checked} onClick={() => console.log("clicou")} />
       <span
-        className={`text-foreground text-[12px] cursor-custom ${checked ? "line-through text-muted-foreground" : ""}`}
+        className={`text-foreground text-[12px] md:text-[18px] ${checked ? "line-through text-muted-foreground" : ""}`}
       >
         TaskItem
       </span>
--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,28 +1,92 @@
+"use client";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { NavbarStatus } from "./navbar-status";
 import { TaskItem } from "./task-item";
 import { Separator } from "./ui/separator";
+import { useState } from "react";
+
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+interface Task {
+  id: string;
+  checked: boolean;
+}
+
+const data = [
+  { id: "1", checked: false },
+  { id: "2", checked: true },
+  { id: "3", checked: false },
+  { id: "4", checked: true },
+  { id: "5", checked: false },
+];
 
 export function TaskList() {
+  const [tasks, setTasks] = useState<Task[]>(data);
   const isMobile = useIsMobile();
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setTasks((items) => {
+        const oldIndex = items.findIndex((task) => task.id === active.id);
+        const newIndex = items.findIndex((task) => task.id === over?.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 
   return (
     <main>
-      <div className="w-full flex flex-col bg-muted-background rounded-lg shadow-xl overflow-auto">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <div key={index}>
-            {index % 2 === 0 ? <TaskItem /> : <TaskItem checked />}
+      <div className="w-full flex flex-col bg-muted-background rounded-lg shadow-xl overflow-hidden">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+            <div>
+              {tasks.map((task, index) => (
+                <>
+                  <TaskItem key={task.id} id={task.id} checked={task.checked} />
+                  {index < tasks.length - 1 && <Separator />}
+                </>
+              ))}
+            </div>
+          </SortableContext>
+        </DndContext>
 
-            {index < 4 && <Separator />}
-          </div>
-        ))}
         <footer className="flex justify-between h-12 w-full">
           <span className="text-[14px] w-[250px] pl-6 flex items-center text-gray-400">
             5 items left
           </span>
           {!isMobile && <NavbarStatus />}
-          <button className="cursor-custom text-[14px] text-foreground w-[250px] pr-6 flex items-center justify-end">
-            Clear Completed
+          <button className="w-[250px] pr-6 flex items-center justify-end cursor-custom">
+            <span className="text-[14px] text-foreground hover:text-muted-foreground">
+              Clear Completed
+            </span>
           </button>
         </footer>
       </div>
