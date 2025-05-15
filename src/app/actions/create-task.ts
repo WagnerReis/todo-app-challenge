701967c79149ee0f4a279dbbd9aa79090b4c99a2
Wagner Reis <wagnerreis1997@gmail.com>
2025-05-15 20:13:39 -0300

    feat(task): implement create task functionality

--- src/app/actions/create-task.ts
+++ src/app/actions/create-task.ts
@@ -1,20 +1,43 @@
 "use server";
 
-import Task, { ITask } from "@/lib/models/Task";
+import Task from "@/lib/models/Task";
 
-export async function createTask() {
+interface CreateTaskInput {
+  description: string;
+  completed: boolean;
+}
+
+interface PrevStateProps {
+  error?: string;
+  success?: string;
+}
+
+export async function createTask(prevState: PrevStateProps, formData: FormData) {
   try {
-    const task: Partial<ITask> = {
-      description: "Estudar react e nextjs",
-      completed: false
+    const description = formData.get("description")?.toString().trim();
+    const checked = formData.get('checked') === 'on';
+
+    if (!description) {
+      return {
+        ...prevState,
+        error: "Campo obrigatório!"
+      };
     }
+
+    const task: CreateTaskInput = {
+      description,
+      completed: checked,
+    };
 
     console.log("creating task...");
 
     await Task.create(task);
 
     console.log("task created successfully");
+
+    return { success: "Tarefa criada com sucesso!" };
   } catch (error) {
     console.error("Erro ao criar tarefa:", error);
+    return { error: "Erro ao criar tarefa" };
   }
 }
--- src/components/task-input.tsx
+++ src/components/task-input.tsx
@@ -1,21 +1,43 @@
+'use client';
+
 import { createTask } from "@/app/actions/create-task";
 import { Button } from "./ui/button";
+import { useFormState } from "react-dom";
+import { useActionState } from "react";
+import { CheckFat } from "@phosphor-icons/react";
 
 export function TaskInput() {
+  const [state, formAction] = useActionState(createTask, { error: "", success: "" });
+
+  console.log(state)
+
   return (
-    <div className="w-full h-16 bg-muted-background rounded-lg pl-6 flex items-center gap-6 mt-10 mb-6">
-      <Button onClick={() => console.log("clicou")} />
+    <form
+      action={formAction}
+      className="w-full h-16 bg-muted-background rounded-lg px-6 flex items-center gap-6 mt-10 mb-6"
+    >
+      {/* TODO: criar component */}
+      <label className="relative w-6 h-6">
+        <input
+          type="checkbox"
+          name="checked"
+          className="peer appearance-none w-6 h-6 border-2 border-gray-300 dark:border-gray-600 checked:border-none rounded-full checked:bg-gradient-to-br from-[var(--brand-start)] to-[var(--brand-end)]"
+        />
+        <CheckFat size={12} color="#fff" weight="fill"
+          className="absolute inset-0 w-4 h-4 m-auto text-white opacity-0 peer-checked:opacity-100 transition-opacity"
+        />
+      </label>
+
       <input
         type="text"
+        name="description"
         placeholder="Create a new todo..."
-        className="text-foreground py-2 outline-none w-full pr-8 text-[12px] md:text-[18px]"
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            createTask();
-          }
-        }}
+        className="text-foreground py-2 outline-none w-[calc(100%-3rem)] text-[12px] md:text-[18px]"
       />
-    </div>
+
+      <button type="submit" className="text-xl">
+        {">"}
+      </button>
+    </form>
   );
 }
